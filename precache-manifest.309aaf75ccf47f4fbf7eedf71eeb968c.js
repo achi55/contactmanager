7d7771bae2b6d8f0222e95abfa9f732cce112c59
achi55 <achivictor52@yahoo.com>
2019-03-15 16:36:19 +0100

    Updates

---
+++ precache-manifest.309aaf75ccf47f4fbf7eedf71eeb968c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "90a40db4f3dc3e2ef935",
+    "url": "/contactmanager/static/js/main.90a40db4.chunk.js"
+  },
+  {
+    "revision": "fcb49eee53ff1302662f",
+    "url": "/contactmanager/static/js/1.fcb49eee.chunk.js"
+  },
+  {
+    "revision": "90a40db4f3dc3e2ef935",
+    "url": "/contactmanager/static/css/main.47d45d28.chunk.css"
+  },
+  {
+    "revision": "fcb49eee53ff1302662f",
+    "url": "/contactmanager/static/css/1.aa643b91.chunk.css"
+  },
+  {
+    "revision": "2b5e4d544982c6f357e13b628225e22a",
+    "url": "/contactmanager/index.html"
+  }
+];
